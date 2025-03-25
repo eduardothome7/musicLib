9240dbfaa6aba7eb0c47152bfca94ca1a96d5264
Eduardo Cannini Thomé <eduardocanninithome@gmail.com>
2025-03-25 15:15:08 -0300

    validates token

--- src/main/java/com/example/musicLib/controller/AuthController.java
+++ src/main/java/com/example/musicLib/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -80,9 +81,26 @@ public class AuthController {
 		}
 	}
 
+	@GetMapping("/validate_token")
+	public ResponseEntity<?> validateToken(HttpServletRequest request) {
+		try {
+
+			if(this.authService.validatesAuthToken(request.getHeader("Token"))) {
+				return ResponseEntity.ok().build();			
+			}
+
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+		} catch (Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(e.getMessage());
+		}
+	}
+
 	@DeleteMapping("/log_out")
 	public void logout(HttpServletRequest request) {
 		this.authService.destroySessionByAuthToken(request.getHeader("Token"));
 	}
-
 }
